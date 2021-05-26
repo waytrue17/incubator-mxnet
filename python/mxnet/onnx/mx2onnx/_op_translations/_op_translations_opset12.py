9ed076ad415b4371427d29e1f58b99160cf1b11e
Wei Chu <weichu@amazon.com>
2021-05-26 14:04:21 -0700

    update where

--- python/mxnet/onnx/mx2onnx/_op_translations/_op_translations_opset12.py
+++ python/mxnet/onnx/mx2onnx/_op_translations/_op_translations_opset12.py
@@ -3719,18 +3719,12 @@ def convert_where(node, **kwargs):
     # have shape (first dim of x,)
     create_tensor([0], name+'_0', kwargs['initializer'])
     create_tensor([1], name+'_1', kwargs['initializer'])
+
     nodes = [
-        make_node('Shape', [input_nodes[0]], [name+'_cond_shape']),
-        make_node('Shape', [name+'_cond_shape'], [name+'_cond_dim']),
-        make_node('Shape', [input_nodes[1]], [name+'_x_shape']),
-        make_node('Shape', [name+'_x_shape'], [name+'_x_dim']),
-        make_node('Sub', [name+'_x_dim', name+'_cond_dim'], [name+'_sub']),
-        make_node('Concat', [name+'_0', name+'_sub'], [name+'_concat'], axis=0),
-        make_node('Pad', [name+'_cond_shape', name+'_concat', name+'_1'], [name+'_cond_new_shape']),
-        make_node('Reshape', [input_nodes[0], name+'_cond_new_shape'], [name+'_cond']),
-        make_node('Cast', [name+'_cond'], [name+'_bool'], to=int(TensorProto.BOOL)),
-        make_node('Where', [name+'_bool', input_nodes[1], input_nodes[2]], [name], name=name)
+        make_node('Cast', [input_nodes[0]], [name+'_bool'], to=int(TensorProto.BOOL)),
+        make_node('Where', [name+'_bool', input_nodes[1], input_nodes[2]], [name], name=name),
     ]
+
     return nodes
 
 
